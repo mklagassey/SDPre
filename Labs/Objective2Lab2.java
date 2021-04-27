fa19a55cf95e3a4992327e86c048d1a768dc2346
mklagassey <mklagassey@gmail.com>
2021-04-26 20:54:17 -0400

    Objective 2 complete

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Mick LaGassey";
+    int age = 41;
+    String state = "Michigan";
+    System.out.print("Name: " + name + " ");
+    System.out.print("Age: " + age + " ");
+    System.out.println("State: " + state);
+  }
+}
